6e49f097119df26c1b5725fa851d421929e5dea2
Ivan Yashin <newparadigma1@gmail.com>
2022-12-10 02:15:26 +0300

    First version of bot. (#2)

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # carmelita
-A telegram bot predicting the future by tarot card.
+A telegram bot predicting the future by tarot cards.
+
+# Инструкция по установке
+1. Копируем docker-compose инструкцию:
+```bash
+cp docker-compose.example.yml docker-compose.yml
+```
+2. Копируем переменные окружения:
+```bash
+cp example.env .env
+```
+3. Заполните переменные окружения в файле .env!
+4. Строим контейнеры
+```bash
+docker-compose build
+```
+5. Запускаем контейнеры
+```bash
+docker-compose up -d
+```
+
+
+# to-do
+1. Генерация предсказаний.
+2. Защита от перебора предсказаний - 1 предсказание на пользователя раз в день. (Колоде нужно отдохнуть)
+3. Реакция бота на тег
---
+++ bot.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+import telebot
+from random import randrange
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['carmelita_bot'])
+# @bot.channel_post_handler(regexp="@carmelita_bot")
+
+def send_cards(message):
+
+	card_numbers = []
+	while(len(card_numbers) < 3):
+		card_number = randrange(77)
+		if card_number not in card_numbers: card_numbers.append(card_number)
+
+	for card_number in card_numbers:
+		file = open(f'tarot_cards/{card_number}.webp', 'rb')
+		bot.send_sticker(message.chat.id, file, reply_to_message_id=message.message_id)
+
+	msg = 'Вот расклад для тебя ✨'
+	bot.send_message(message.chat.id, msg, reply_to_message_id=message.message_id)
+
+bot.infinity_polling()

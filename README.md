83dd0ef27e4b1d448beb26b4b52eebfa56cf5284
Ivan Yashin <newparadigma1@gmail.com>
2022-12-10 01:24:13 +0300

    Init bot and docker prod/dev env.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # carmelita
-A telegram bot predicting the future by tarot card.
+A telegram bot predicting the future by tarot cards.
+
+# Инструкция по установке
+1. Копируем docker-compose инструкцию:
+```bash
+cp docker-compose.example.yml docker-compose.yml
+```
+2. Копируем переменные окружения:
+```bash
+cp example.env .env
+```
+3. Заполните переменные окружения в файле .env!
+4. Строим контейнеры
+```bash
+docker-compose build
+```
+5. Запускаем контейнеры
+```bash
+docker-compose up -d
+```
---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+import telebot
+from random import randrange
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+bot = telebot.TeleBot("BOT_TOKEN")
+
+@bot.message_handler(commands=['start'])
+# @bot.channel_post_handler(regexp="@carmelita_bot")
+
+def send_cards(message):
+
+	card_numbers = []
+	while(len(card_numbers) < 3):
+		card_number = randrange(77)
+		if card_number not in card_numbers: card_numbers.append(card_number)
+
+	for card_number in card_numbers:
+		msg = ''
+		file = open(f'tarot_cards/{card_number}.webp', 'rb')
+		bot.send_photo(message.chat.id, file, msg, reply_to_message_id=message.message_id)
+
+bot.infinity_polling()

e88402804dfb0d1880c0d5ac573fb1729834c63f
Ivan Yashin <newparadigma1@gmail.com>
2024-05-15 15:50:38 +0700

    result

--- src/model/Prediction.py
+++ src/model/Prediction.py
@@ -1,6 +1,12 @@
 class Prediction:
-    def get_prediction_text(self):
-        msg =  "Вот расклад для тебя ✨\n\n"
-        msg =  msg + str(order) + '. ' + card_names[card_number].capitalize() + "\n"
-        msg += "\n" + "✨✨✨✨✨✨✨✨" 
-        return msg
+    def __init__(self, cards):
+        self.cards = cards
+        self.text1 = "Вот расклад для тебя ✨\n\n"
+        self.text2 = "\n" + "✨✨✨✨✨✨✨✨"
+
+    def __repr__(self): 
+        text = self.text1
+        for i in range(len(self.cards)):
+            text += str(i + 1) + '. ' + self.cards[i].capitalize() + "\n"
+        text += self.text2
+        return text
